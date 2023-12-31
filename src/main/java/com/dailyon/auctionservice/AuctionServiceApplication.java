87f5b1cae3728d75c374c1fd385c47759ae710ce
전종민 <ho05248@naver.com>
2023-12-31 22:39:36 +0900

    [FIX] time zone

--- src/main/java/com/dailyon/auctionservice/AuctionServiceApplication.java
+++ src/main/java/com/dailyon/auctionservice/AuctionServiceApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -14,6 +17,11 @@ public class AuctionServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuctionServiceApplication.class, args);
+    }
+
+    @PostConstruct
+    public void setTimezoneToSeoul() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 
 }
