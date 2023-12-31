1891037f0d3681e9dca26244fac5c498567dd818
전종민 <ho05248@naver.com>
2023-12-31 22:38:47 +0900

    [CHORE] config about dynamodb

--- src/main/java/com/dailyon/auctionservice/AuctionServiceApplication.java
+++ src/main/java/com/dailyon/auctionservice/AuctionServiceApplication.java
@@ -1,11 +1,15 @@
 package com.dailyon.auctionservice;
 
+import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableDynamoDBRepositories
 public class AuctionServiceApplication {
 
     public static void main(String[] args) {
