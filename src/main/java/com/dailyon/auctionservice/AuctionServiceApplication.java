ec2e9cc6de0ed973716448a4edac154fe6400119
wakkpu <ho0524870@gmail.com>
2024-01-02 19:59:17 +0900

    [FIX] create-drop dynamodb

--- src/main/java/com/dailyon/auctionservice/AuctionServiceApplication.java
+++ src/main/java/com/dailyon/auctionservice/AuctionServiceApplication.java
@@ -1,5 +1,12 @@
 package com.dailyon.auctionservice;
 
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsync;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.util.TableUtils;
+import com.dailyon.auctionservice.document.Auction;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -12,6 +19,11 @@ import java.util.TimeZone;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class AuctionServiceApplication {
+    @Autowired
+    AmazonDynamoDBAsync dynamoDB;
+
+    @Autowired
+    DynamoDBMapper dynamoDBMapper;
 
     public static void main(String[] args) {
         SpringApplication.run(AuctionServiceApplication.class, args);
@@ -20,5 +32,17 @@ public class AuctionServiceApplication {
     @PostConstruct
     public void setTimezoneToSeoul() {
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+
+    // TODO : document FIX 후 삭제
+    @PostConstruct
+    public void setDynamoDB() {
+        TableUtils.deleteTableIfExists(dynamoDB, dynamoDBMapper.generateDeleteTableRequest(Auction.class));
+
+        CreateTableRequest createTableRequest = dynamoDBMapper
+                .generateCreateTableRequest(Auction.class)
+                .withProvisionedThroughput(new ProvisionedThroughput(1L, 1L));
+
+        TableUtils.createTableIfNotExists(dynamoDB, createTableRequest);
     }
 }
