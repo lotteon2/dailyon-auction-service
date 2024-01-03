fadbcc2fcf98520efb1f5f1ede6b7e5e09a7bffa
wakkpu <ho0524870@gmail.com>
2024-01-03 15:50:01 +0900

    O2F-1061 [FEAT] 경매 목록 조회

--- src/main/java/com/dailyon/auctionservice/service/AuctionService.java
+++ src/main/java/com/dailyon/auctionservice/service/AuctionService.java
@@ -5,8 +5,15 @@ import com.dailyon.auctionservice.document.Auction;
 import com.dailyon.auctionservice.dto.request.CreateAuctionRequest;
 import com.dailyon.auctionservice.repository.AuctionRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -25,5 +32,27 @@ public class AuctionService {
                         auctionRequest.getStartAt()
                 )
         );
+    }
+
+    public Page<Auction> readAuctions(Pageable pageable) {
+        int currentPage = pageable.getPageNumber();
+        int pageSize = pageable.getPageSize();
+
+        int startIdx = currentPage * pageSize;
+        int endIdx = startIdx + pageSize;
+
+        List<Auction> auctions = (List<Auction>) auctionRepository.findAll();
+        if(auctions.isEmpty()) {
+            return new PageImpl<>(new ArrayList<>(), pageable, 0);
+        }
+
+        int totalSize = auctions.size();
+
+        List<Auction> sorted = auctions.stream()
+                .sorted(Auction::compareTo)
+                .collect(Collectors.toList())
+                .subList(startIdx, Math.min(endIdx, totalSize));
+
+        return new PageImpl<>(sorted, pageable, totalSize);
     }
 }
---
+++ src/test/java/com/dailyon/auctionservice/service/AuctionServiceTests.java
@@ -0,0 +1,76 @@
+package com.dailyon.auctionservice.service;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBAsync;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.util.TableUtils;
+import com.dailyon.auctionservice.ContainerBaseTestSupport;
+import com.dailyon.auctionservice.document.Auction;
+import com.dailyon.auctionservice.repository.AuctionRepository;
+import org.assertj.core.api.BDDAssertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDateTime;
+import java.util.stream.IntStream;
+
+public class AuctionServiceTests extends ContainerBaseTestSupport {
+    @Autowired private AmazonDynamoDBAsync dynamoDB;
+    @Autowired private DynamoDBMapper dynamoDBMapper;
+    @Autowired private AuctionRepository auctionRepository;
+    @Autowired private AuctionService auctionService;
+
+    @BeforeEach
+    void beforeEach() {
+        CreateTableRequest createTableRequest = dynamoDBMapper
+                .generateCreateTableRequest(Auction.class)
+                .withProvisionedThroughput(new ProvisionedThroughput(1L, 1L));
+
+        TableUtils.createTableIfNotExists(dynamoDB, createTableRequest);
+    }
+
+    @AfterEach
+    void afterEach() {
+        TableUtils.deleteTableIfExists(
+                dynamoDB,
+                dynamoDBMapper.generateDeleteTableRequest(Auction.class)
+        );
+    }
+
+    @Test
+    @DisplayName("경매 목록 생성 내림차순 기준 정렬 페이지네이션 조회")
+    void paginationTest() {
+        for(int i=0; i<10; i++) {
+            auctionRepository.save(Auction.builder()
+                    .auctionProductId((long) i)
+                    .auctionName("TEST_"+i)
+                    .startBidPrice(1000)
+                    .maximumWinner(5)
+                    .startAt(LocalDateTime.now())
+                    .build()
+            );
+        }
+
+        Page<Auction> auctions = auctionService.readAuctions(PageRequest.of(0, 5));
+        assertEquals(10, auctions.getTotalElements());
+        assertEquals(2, auctions.getTotalPages());
+        assertEquals(5, auctions.getContent().size());
+        IntStream.range(1, 5).forEach(i -> {
+            Auction prev = auctions.getContent().get(i-1);
+            Auction next = auctions.getContent().get(i);
+
+            BDDAssertions
+                    .then(prev.getCreatedAt().isAfter(next.getCreatedAt()))
+                    .isTrue();
+        });
+    }
+}
