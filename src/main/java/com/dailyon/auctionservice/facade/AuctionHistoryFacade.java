ed5023b24bbdc54ca9a61cf1f9c17727ea3eace7
전종민 <ho05248@naver.com>
2024-01-16 23:52:00 +0900

    [FEAT] response

--- src/main/java/com/dailyon/auctionservice/facade/AuctionHistoryFacade.java
+++ src/main/java/com/dailyon/auctionservice/facade/AuctionHistoryFacade.java
@@ -1,6 +1,7 @@
 package com.dailyon.auctionservice.facade;
 
 import com.dailyon.auctionservice.document.AuctionHistory;
+import com.dailyon.auctionservice.dto.response.ReadAuctionHistoryPageResponse;
 import com.dailyon.auctionservice.service.AuctionHistoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -12,7 +13,7 @@ import org.springframework.stereotype.Component;
 public class AuctionHistoryFacade {
     private final AuctionHistoryService auctionHistoryService;
 
-    public Page<AuctionHistory> readAuctionHistories(String memberId, Pageable pageable) {
-        return auctionHistoryService.readAuctionHistories(memberId, pageable);
+    public ReadAuctionHistoryPageResponse readAuctionHistories(String memberId, Pageable pageable) {
+        return ReadAuctionHistoryPageResponse.of(auctionHistoryService.readAuctionHistories(memberId, pageable));
     }
 }
